46509e936ee9991599ada20beec31fb661aba7eb
SasaPahor <sasa.pahor@studenti.units.it>
2024-02-21 12:37:21 +0000

    Esercizio 3 completato

---
+++ esercizi/somma_csv.py
@@ -0,0 +1,8 @@
+def sum_csv(file_name):
+    somma=0
+    my_file=open(file_name)
+    for line in my_file:
+        elements=line.split(',')
+        if(elements[0]!='Date'):
+            somma+=float(elements[1])
+    return somma
