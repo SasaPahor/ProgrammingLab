e023ed0ecb26dc1a1b9bc45ac942e38658672f1a
SasaPahor <sasa.pahor@studenti.units.it>
2024-02-21 13:48:09 +0000

    Sum_csv

--- esercizi/parte3.py
+++ esercizi/parte3.py
@@ -8,6 +8,7 @@ def sum_csv(file_name):
             elements=line.split(',')
             if(elements[0]!='Date'):
                 somma+=float(elements[1])
+    my_file.close()
     return somma
 #Test utilizzo
 #print(sum_csv('shampo_sales.csv'))
